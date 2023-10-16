cc229790e6f9b3ce09d74823bc6ee967716bf7c7
thibault livran <thibault.livran@dnd.fr>
2023-10-16 11:30:20 +0200

    HomePage: redirect each movies and actors to an detailled page

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/FicheMovie/:id',
+      name: 'FicheMovie',
+      component: () => import('../views/FicheMovieView.vue')
+    },
+    {
+      path: '/FicheActor/:id',
+      name: 'FicheActor',
+      component: () => import('../views/FicheActorView.vue')
     }
   ]
 })
---
+++ src/views/FicheActorView.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { onMounted } from 'vue'
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { urlBase } from '@/main.js';
+import ActorCard from '@/components/ActorCard.vue';
+
+const route = useRoute()
+let actorInfo = ref('')
+
+onMounted(async () => {
+  const id = route.params.id
+  console.log(id)
+  const response = await fetch(`${urlBase}/S5-TD1/index.php/api/actors/${id}`);
+  actorInfo.value = await response.json()
+  console.log(actorInfo)
+})
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is the info page</h1>
+  </div>
+
+  <div class="actors">
+    <h2>Hello Actor</h2>
+    <div>
+      <ActorCard :actor="actorInfo" v-if="actorInfo" />
+
+      <h3>Les Films de l'Acteur : </h3>
+      <ul v-for="movie in actorInfo.movies" :key="actor">
+        <router-link :to="{ name: 'FicheMovie', params: { id: movie.id } }">
+          <li>
+            {{ movie.title }}
+          </li>
+        </router-link>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    display: flex;
+  }
+}
+</style>
