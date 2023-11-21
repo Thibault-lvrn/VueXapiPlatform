398c1ec133e4ee6bf46215852d7562277753eef5
thibault livran <thibault.livran@dnd.fr>
2023-11-21 10:05:43 +0100

    CategoriesPage: categories pages layout rebase

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -4,13 +4,6 @@ import MovieCard from '@/components/MovieCard.vue';
 import { urlBase } from '@/main.js';
 
 let categories = ref([]);
-// let urlBase = "http://localhost:8088"
-// let lefilm = ref([]);
-
-// const headers = {
-//   Accept: "application/json",
-//   "Content-Type": "application/json",
-// };
 
 const fetchActors = async () => {
   const response = await fetch(`${urlBase}/S5-TD1/index.php/api/categories?page=1`);
@@ -26,22 +19,24 @@ fetchActors();
     <h1>This is the Categories page</h1>
   </div>
 
-  <div class="actors">
+  <div class="category">
     <h2>Categories</h2>
-    <ul>
-      <li v-if="categories" v-for="categorie in categories['hydra:member']" :key="categorie.id">
-        <router-link :to="{ name: 'FicheCategory', params: { id: categorie.id } }">
-          {{ categorie.name }}
-        </router-link>
+    <div>
+      <div v-if="categories" v-for="categorie in categories['hydra:member']" :key="categorie.id">
+        <h3>
+          <router-link :to="{ name: 'FicheCategory', params: { id: categorie.id } }">
+            {{ categorie.name }}
+          </router-link>
+        </h3>
         <ul>
-          <li v-for="film in categorie.movies" :key="film.id">
+          <li class="card" v-for="film in categorie.movies" :key="film.id">
             <router-link :to="{ name: 'FicheMovie', params: { id: film.id } }">
               <MovieCard :film="film" v-if="film" />
             </router-link>
           </li>
         </ul>
-      </li>
-    </ul>
+      </div>
+    </div>
   </div>
 </template>
 
