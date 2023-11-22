1bae02ad7d6c17717502e4a4ddf3af6ba556288f
thibault livran <thibault.livran@dnd.fr>
2023-11-22 08:09:21 +0100

    Login: Add new login page with JWT token

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+
+let user = localStorage.getItem('user');
+
+function logout() {
+  localStorage.removeItem('token');
+  localStorage.removeItem('user');
+  router.push('/')
+}
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
+    <div class="wrapper navigation">
       <!-- <HelloWorld msg="You did it!" /> -->
 
       <nav>
@@ -16,10 +25,20 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/actors">Actors</RouterLink>
         <RouterLink to="/categories">Categories</RouterLink>
       </nav>
+      
+      <div class="secondary-nav">
+        <div class="logout">{{ user }}</div>
+        <template v-if="!user">
+          <RouterLink to="/login">Login</RouterLink>
+        </template>
+        <template v-if="user">
+          <button @click="logout()">Logout</button>
+        </template>
+      </div>
     </div>
   </header>
 
-  <RouterView />
+  <RouterView :key="$router.path"/>
 </template>
 
 <style scoped>
@@ -27,58 +46,5 @@ header {
   line-height: 1;
   max-height: 100vh;
   padding: 20px 0
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-  }
-
-  .logo {
-    margin: 0;
-    height: 40px;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    font-size: 1rem;
-    padding: 1rem 0;
-  }
 }
 </style>
