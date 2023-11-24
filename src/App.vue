acd3f1eda4f70ca340f2e94a1f5fa73975b5c745
thibault livran <thibault.livran@dnd.fr>
2023-11-24 09:12:54 +0100

    Login: refresh pages data on login/logout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, watchEffect, ref } from 'vue';
+import { useRouter } from 'vue-router';
 
+const isAuthenticated = ref(false);
+const user = ref(localStorage.getItem('username') || '');
+let showLoginLink = ref(true);
 
-let user = localStorage.getItem('user');
+const checkToken = () => {
+  const token = localStorage.getItem('token');
+  isAuthenticated.value = !!token;
+  user.value = localStorage.getItem('user') || '';
+};
 
-function logout() {
+const logout = () => {
   localStorage.removeItem('token');
   localStorage.removeItem('user');
-  router.push('/')
-}
+  isAuthenticated.value = false;
+  user.value = '';
+};
+
+
+onMounted(() => {
+  checkToken();
+  watchEffect(() => {
+    checkToken();
+  });
+});
+
+const router = useRouter();
+
+router.beforeEach((to, from, next) => {
+  checkToken();
+  next();
+});
 </script>
 
 <template>
@@ -27,13 +51,20 @@ function logout() {
       </nav>
       
       <div class="secondary-nav">
-        <div class="logout">{{ user }}</div>
-        <template v-if="!user">
+        <!-- <template v-if="!showLogin">
+          <p class="logout">{{ user }}</p>
+        </template>
+        <template v-if="showLogin">
           <RouterLink to="/login">Login</RouterLink>
         </template>
-        <template v-if="user">
+        <template v-if="!showLogin">
           <button @click="logout()">Logout</button>
-        </template>
+        </template> -->
+        <!-- <button @click="test()">test</button>
+        <p v-text="showLogin"></p> -->
+        <router-link v-if="!isAuthenticated" class="link-nav-menu" to="/login">Login</router-link>
+        <router-link v-if="isAuthenticated" @click="logout" class="link-nav-menu" to="/">Logout</router-link>
+        <router-link v-if="isAuthenticated" class="link-nav-menu" to="/account">{{user}}</router-link>
       </div>
     </div>
   </header>
