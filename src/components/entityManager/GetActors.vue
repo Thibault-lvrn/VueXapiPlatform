fa3216313978f8440c83f383e7c5141a5f2bb751
thibault livran <thibault.livran@dnd.fr>
2024-02-13 22:21:49 +0100

    MovieEdition: add get actor component

---
+++ src/components/entityManager/GetActors.vue
@@ -0,0 +1,24 @@
+<script>
+import axios from 'axios';
+
+export default {
+  methods: {
+    fetchDataWithAuthorization() {
+      const config = {
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        }
+      };
+
+      return axios.get('http://localhost:8088/WRA506/index.php/api/actors', config)
+        .then(response => {
+          return response.data;
+        })
+        .catch(error => {
+          console.error(error);
+          throw error; // relancer l'erreur pour la gérer ailleurs si nécessaire
+        });
+    }
+  }
+};
+</script>
