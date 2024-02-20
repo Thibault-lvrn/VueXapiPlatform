6ad1b349bc18fbc6c49d9cf6ec7c6b1e23caffed
thibault livran <thibault.livran@dnd.fr>
2024-02-20 11:30:09 +0100

    CategoryEdition: create category editions components

---
+++ src/components/entityManager/GetCategory.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios';
+import { urlBase } from '@/main.js';
+
+export default {
+  methods: {
+    getCategory() {
+      const config = {
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        }
+      };
+
+      return axios.get(`${urlBase}/api/categories`, config)
+        .then(response => {
+          return response.data;
+        })
+        .catch(error => {
+          console.error(error);
+          throw error; 
+        });
+    }
+  }
+};
+</script>
