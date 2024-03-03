f26a9be78b4bdefb9a0425b3f3e6c1a23c7fec31
thibault livran <thibault.livran@dnd.fr>
2024-03-03 23:41:29 +0100

    ActorEdition: Add GetNationality component

---
+++ src/components/entityManager/GetNationality.vue
@@ -0,0 +1,20 @@
+<script>
+import axios from 'axios';
+import { urlBase } from '@/main.js';
+
+export default {
+  methods: {
+    getNationality() {
+
+      return axios.get(`${urlBase}/api/nationalities`)
+        .then(response => {
+          return response.data;
+        })
+        .catch(error => {
+          console.error(error);
+          throw error; 
+        });
+    }
+  }
+};
+</script>
