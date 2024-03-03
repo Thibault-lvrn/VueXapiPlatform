5082eb22cfb6d1dc2c15d68a017425debfe733ba
thibault livran <thibault.livran@dnd.fr>
2024-03-03 23:17:16 +0100

    CategoryEdition: Add and update category components

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -4,22 +4,19 @@ import MovieCard from '@/components/Card/MovieCard.vue';
 import { urlBase } from '@/main.js';
 import axios from 'axios';
 import AddCategory from '@/components/entityManager/AddCategory.vue';
+import RemoveCategory from '@/components/entityManager/RemoveCategory.vue';
+import UpdateCategory from '@/components/entityManager/UpdateCategory.vue';
+import UpdateCategoryMovie from '@/components/entityManager/UpdateCategoryMovie.vue';
 
-let categories = ref([]);
+const categories = ref([]);
 const isLoading = ref(true);
-const pageNumber = ref(parseInt(localStorage.getItem('categoryPageNumber')) || 1);
-const numberOfPages = ref('');
-const itemsPerPage = ref(2);
 const searchTerm = ref('');
+const activeCategory = ref(null);
 
-
-const fetchCategories = async (page) => {
-  if (page) {
-    pageNumber.value = page;
-  }
+const fetchCategories = async () => {
   try {
     const response = await axios.get(
-      `${urlBase}/api/categories?page=${pageNumber.value}&itemsPerPage=${itemsPerPage.value}`
+      `${urlBase}/api/categories`
     );
 
     if (!response.data) {
@@ -27,7 +24,6 @@ const fetchCategories = async (page) => {
     }
 
     categories.value = response.data;
-    numberOfPages.value = Math.ceil(response.data['hydra:totalItems'] / itemsPerPage.value);
     isLoading.value = false;
   } catch (error) {
     console.error('An error occurred while fetching categories:', error);
@@ -50,24 +46,12 @@ const searchCategories = async () => {
   }
 };
 
-const nextPage = () => {
-  if (pageNumber.value >= numberOfPages.value) {
-    pageNumber.value = numberOfPages.value;
+const toggleCategory = (categoryId) => {
+  if (activeCategory.value === categoryId) {
+    activeCategory.value = null;
   } else {
-    pageNumber.value++;
-    localStorage.setItem('categoryPageNumber', pageNumber.value);
+    activeCategory.value = categoryId;
   }
-  fetchCategories();
-};
-
-const previousPage = () => {
-  if (pageNumber.value <= 1) {
-    pageNumber.value = 1;
-  } else {
-    pageNumber.value--;
-    localStorage.setItem('categoryPageNumber', pageNumber.value);
-  }
-  fetchCategories();
 };
 
 onMounted(fetchCategories);
@@ -80,44 +64,51 @@ onMounted(fetchCategories);
     <form class="search-container">
       <input type="text" v-model="searchTerm" placeholder="Rechercher des catégories..." @input="searchCategories">
     </form>
-    <div>
-      <ul class="pagination">
-        <button class="pagination_btn" @click="previousPage()">&lt;</button>
-        <template v-for="page in numberOfPages">
-          <button class="pagination_btn" :class="{ active: pageNumber === page }" @click="fetchCategories(page)">
-            <span>{{ page }}</span>
-          </button>
-        </template>
-        <button class="pagination_btn" @click="nextPage()">></button>
-      </ul>
-    </div>
     <div class="addEntity">
       <div>
         <button @click="AddCategory.methods.openModalAdd()">Ajouter une catégories</button>
       </div>
+      <AddCategory/>
     </div>
     <div>
       <div class="loading" v-if="isLoading">
         Chargement...
       </div>
-      <div v-if="categories" v-for="categorie in categories['hydra:member']" :key="categorie.id">
-        <h3>
-          <router-link :to="{ name: 'FicheCategory', params: { id: categorie.id } }">
-            {{ categorie.name }}
-          </router-link>
-        </h3>
+      <div v-if="categories" v-for="category in categories['hydra:member']" :key="category.id" :class="'categoryCard-' + category.id">
+        <div>
+          <UpdateCategory :category="category" />
+          <UpdateCategoryMovie :category="category" />
+          <RemoveCategory :categoryId="category.id" :hasMovies="category.movies"/>
+          <h3 class="category-name">
+            <div class="expand-button" :class="{ active: activeCategory === category.id }" @click="toggleCategory(category.id)">
+              <span class="gesture expand material-symbols-outlined">
+                expand_more
+              </span>
+              {{ category.name }}
+            </div>
+            <div class="category-gesture">
+              <button class="gesture remove material-symbols-outlined" v-on:click="UpdateCategoryMovie.methods.openModalAddMovie(category.id)">
+                add
+              </button>
+              <button class="gesture edit material-symbols-outlined" v-on:click="UpdateCategory.methods.openModalEdit(category.id)">
+                edit
+              </button>
+              <button class="gesture remove material-symbols-outlined" v-on:click="RemoveCategory.methods.openModalRemove(category.id)">
+                delete
+              </button>
+            </div>
+          </h3>
+        </div>
         <ul class="item-listing">
-          <div class="loading" v-if="isLoading">
-            Chargement...
-          </div>
-          <li v-if="!isLoading" class="card card-movie" v-for="movie in categorie.movies" :key="movie.id">
-            <MovieCard :movie="movie" v-if="movie" />
+          <li class="card card-movie" v-for="movie in category.movies" :key="movie.id"  v-if="activeCategory === category.id && !isLoading">
+            <MovieCard :movie="movie" callerComponent="CategoriesView" v-if="movie" />
           </li>
         </ul>
       </div>
     </div>
   </div>
 </template>
+
 
 <style>
 @media (min-width: 1024px) {
